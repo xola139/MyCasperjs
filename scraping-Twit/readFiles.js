d4bcabfc6db48a48be52d8e267d1d9967ea86530
Ruflex <ulfixdev@localhost.localdomain>
2017-11-17 14:56:26 -0600

    update with save mongolab

--- scraping-Twit/readFiles.js
+++ scraping-Twit/readFiles.js
@@ -1,47 +1,67 @@
 //http://www.tutorialsteacher.com/nodejs/access-mongodb-in-nodejs
+//http://www.tutorialsteacher.com/nodejs/access-mongodb-in-nodejs
+//https://stackoverflow.com/questions/6831918/node-js-read-a-text-file-into-an-array-each-line-an-item-in-the-array
+//http://blog.koalite.com/2011/10/javascript-diferencias-entre-declaracion-de-funcion-y-expresion-con-funcion/
+//http://www.etnassoft.com/2011/09/02/funciones-declaradas-vs-funciones-expresadas-en-javascript/
 
-/*
+
 var MongoClient = require('mongodb').MongoClient;
 
-// Connect to the db
-MongoClient.connect("mongodb://usuariogeneric:sharkesink139@ds041603.mlab.com:41603/generic", function (err, db) {
-    
-    db.collection('usuarios', function (err, collection) {
-        
-        /*collection.find().toArray(function(err, items) {
-            if(err) throw err;    
-            console.log(items);            
-        });*/
+var saveData = function (data){
+	// Connect to the db
+	MongoClient.connect("mongodb://usuariogeneric:sharkesink139@ds041603.mlab.com:41603/generic", function (err, db) {
+	    
+	    db.collection('images', function (err, collection) {
+	        
+	        /*collection.find().toArray(function(err, items) {
+	            if(err) throw err;    
+	            console.log(items);            
+	        });*/
 
-  /*       collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
-        
-        
+	        collection.insert(data);
+	        db.close();
+	        
+	        
 
-        /*db.collection('Persons').count(function (err, count) {
-            if (err) throw err;
-            
-            console.log('Total Rows: ' + count);
-        });*/
-        
-   // });
-                
-//});
+	        /*db.collection('Persons').count(function (err, count) {
+	            if (err) throw err;
+	            
+	            console.log('Total Rows: ' + count);
+	        });*/
+	        
+	    });
+	                
+	});
 
+return;
+};
 
 
 
 var fs = require('fs');
-var file = __dirname + '/scraped_data/sopitas.json';
 
-fs.readFile(file, 'utf8', function (err, data) {
-  if (err) {
-    console.log('Error: ' + err);
-    return;
-  }
+var readFile = function(name){
+	var file = __dirname + '/scraped_data/'+ name +'.json';
 
-  data = JSON.parse(data);
+	fs.readFile(file, 'utf8', function (err, data) {
+		if (err) {
+			console.log('Error: ' + err);
+	    	return;
+	  	}
 
-  console.log(data);
+	  	data = JSON.parse(data);
+		saveData(data);
+	});
+};
+
+
+fs.readFile(__dirname + '/links.txt', function(err, f){
+    var array = f.toString().split('\n');
+    for(i in array) {
+    	readFile(array[i].replace(/(\n|\r)+$/, ''));
+    }
 });
+
+
+
+
